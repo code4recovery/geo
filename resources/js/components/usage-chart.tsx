ebebd8b0ced6965af824384f35be57b84d4497e9
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2025-09-20 07:56:49 -0700

    format chart

--- resources/js/components/usage-chart.tsx
+++ resources/js/components/usage-chart.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Bar, BarChart, CartesianGrid, XAxis } from 'recharts';
+import { Bar, BarChart, CartesianGrid, ResponsiveContainer, XAxis, YAxis } from 'recharts';
 
 import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart';
 
@@ -17,12 +17,15 @@ type ChartData = {
 };
 
 export const UsageChart = ({ chart_data }: { chart_data: ChartData[] }) => (
-    <ChartContainer config={chartConfig} className="h-full min-h-[200px] w-full">
-        <BarChart accessibilityLayer data={chart_data}>
-            <CartesianGrid vertical={false} />
-            <XAxis dataKey="month" tickLine={false} tickMargin={10} axisLine={false} tickFormatter={(value) => value.slice(0, 3)} />
-            <ChartTooltip content={<ChartTooltipContent className="min-w-[150px]" />} />
-            <Bar dataKey="geocodes" fill="var(--color-geocodes)" radius={4} isAnimationActive={false} />
-        </BarChart>
-    </ChartContainer>
+    <ResponsiveContainer>
+        <ChartContainer config={chartConfig} className="h-full min-h-[200px] w-full">
+            <BarChart accessibilityLayer data={chart_data}>
+                <CartesianGrid vertical={false} />
+                <XAxis dataKey="month" tickLine={false} tickMargin={10} axisLine={false} tickFormatter={(value) => value.slice(0, 3)} />
+                <YAxis axisLine={false} tickLine={false} tickFormatter={(value) => (value ? `${value / 1000}k` : '0')} />
+                <ChartTooltip content={<ChartTooltipContent className="min-w-[150px]" />} />
+                <Bar dataKey="geocodes" fill="var(--color-geocodes)" radius={4} isAnimationActive={false} />
+            </BarChart>
+        </ChartContainer>
+    </ResponsiveContainer>
 );
